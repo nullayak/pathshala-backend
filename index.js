bd656f180f1f7b645a46d8d879f34c77dff61b95
Aditya Prakash <p.aditya.198@gmail.com>
2020-06-21 11:46:12 +0530

    cors

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(express.static(__dirname + "/"));
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 http.listen(8080);
 console.log("PORT is 8080");
 var fs = require("fs");
